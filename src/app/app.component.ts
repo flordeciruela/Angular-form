61cf4c398f8a8931f1330e6680a823dbc64e6d04
wendyrs <wreyes@phantasia.pe>
2017-12-13 16:54:23 -0500

    adding strings to array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,26 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  title = 'Cadaver Exquisito';
   form: FormGroup;
   fb: FormBuilder;
+  arr = ["¡Quiero torta!",
+             "La niña baila marinera en el patio",
+             "Pensando en lo mismo",
+             "Me gusta la música",
+             "Camaron que se duerme se lo lleva la corriente",
+             "Laboratoria es código que transforma",
+             "Todo va a estar bien",
+             "Me encanta bailar para relajarme"];
 
   constructor(fb: FormBuilder) {
     this.fb = fb;
+  }
+
+  addString(newString: string) {
+    if (newString) {
+      this.arr.push(newString);
+    }
   }
 
   ngOnInit(){
@@ -19,4 +34,5 @@ export class AppComponent implements OnInit {
       name: ['']
     })
   }
+
 }
