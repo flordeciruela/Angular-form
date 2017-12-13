3f2c300477f3d34e1da4669f564aff3069fe2a93
wendyrs <wreyes@phantasia.pe>
2017-12-13 15:12:36 -0500

    reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  fb: FormBuilder;
+
+  constructor(fb: FormBuilder) {
+    this.fb = fb;
+  }
+
+  ngOnInit(){
+    this.form = this.fb.group({
+      name: ['']
+    })
+  }
 }
