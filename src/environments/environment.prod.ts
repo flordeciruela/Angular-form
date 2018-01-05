2e6f0d8119ff6c5484895a96118650b9137aa277
katherineortega <katy_ja2915@hotmail.com>
2018-01-04 20:10:38 -0500

    version firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBKsjpYUqdqDTTE3UGwZI2B80y_IirnYIA',
+    authDomain: 'angular-corpse-game.firebaseapp.com',
+    databaseURL: 'https://angular-corpse-game.firebaseio.com',
+    projectId: 'angular-corpse-game',
+    storageBucket: 'angular-corpse-game.appspot.com',
+    messagingSenderId: '481710050138'
+  }
 };
